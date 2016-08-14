397b1cc03dd735304bf0dd0df1dad057998be483
jshyong-nisum-com <jshyong@nisum.com>
2016-08-14 13:20:23 -0700

    change test

--- src/test/java/test/jenkins/hello/HelloWorldTest.java
+++ src/test/java/test/jenkins/hello/HelloWorldTest.java
@@ -7,19 +7,23 @@ import org.junit.Test;
 public class HelloWorldTest {
 
 	@Test
-	public void shouludPassFirstHello() {
+	public void shouldPassFirstHello() {
 		System.out.println("Should pass 1st hello world at present.");
 	}
 	
 	@Test
-	public void shouludPassSecondHello() {
+	public void shouldPassSecondHello() {
 		System.out.println("Should pass 2nd hello world at present.");
 	}
 
 	@Test
-	public void shouludFailHello() {
-		System.out.println("Should fail hello world at present.");
-		fail("*Error at fail hello !!!");
+	public void shouldRunHelloAsExecpted() {
+		boolean isFail = false;
+		if (isFail) {
+			System.out.println("Should fail hello world at present.");
+			fail("*Error at fail hello !!!");
+		}
+		System.out.println("Should pass hello world as expected.");
 	}
 	
 }
